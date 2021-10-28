6b7354827d436a24914b0bf4a967680d5ad963ba
Thiago Alves <thiago.balves@hotmail.com>
2021-10-28 17:09:17 -0300

    adicionadosrecursos de deletar nota e inclusao de categorias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListaDeNotas from './componentes/ListaDeNotas/';
 import FormularioCadastro from './componentes/FormularioCadastro/';
+import ListaDeCategorias from './componentes/ListaDeCategorias';
 import './assets/App.css';
 import './assets/index.css';
 
@@ -10,7 +11,8 @@ class App extends Component {
     super();
 
     this.state = {
-      notas:[]
+      notas:[],
+      categorias:[],
     };
   }
 
@@ -23,11 +25,33 @@ criarNota(titulo, texto) {
   this.setState(novoEstado);
 }
 
+deletarNota(index){
+  let arrayDeNotas = this.state.notas;
+  arrayDeNotas.splice(index,1);
+  this.setState({notas:arrayDeNotas})
+}
+
+adicionarCategoria(nomeCategoria){
+  const novoArrayCategorias =[...this.state.categorias, nomeCategoria];
+  const novoEstado = {...this.state, categorias: novoArrayCategorias};
+  this.setState(novoEstado)
+}
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <main className="conteudo-principal"> 
+          <ListaDeCategorias
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.state.categorias}
+          />
+
+          <ListaDeNotas 
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}/>          
+        </main>
+       
       </section>
       
       );
